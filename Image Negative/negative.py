af957f64407c7a99a80ed50e08bd771cf1b73063
mohammaduzair9 <mohduzair9@gmail.com>
2018-04-02 12:18:02 +0500

    Negative for rgb, grey scale and binary images

--- Image Negative/negative.py
+++ Image Negative/negative.py
@@ -6,18 +6,27 @@ import numpy as np
 S = 255
 
 # if in rgb
-# open in rgb
-img = cv2.imread(sys.argv[1],cv2.IMREAD_COLOR)    
-B,G,R = cv2.split(img)
-B[:] = [S-x for x in B]     #inverting blue
-G[:] = [S-x for x in G]     #inverting green    
-R[:] = [S-x for x in R]     #inverting red
+if(sys.argv[2]=="rgb"):
+    # open in rgb
+    img = cv2.imread(sys.argv[1],cv2.IMREAD_COLOR)    
+    B,G,R = cv2.split(img)
+    B[:] = [S-x for x in B]     #inverting blue
+    G[:] = [S-x for x in G]     #inverting green    
+    R[:] = [S-x for x in R]     #inverting red
 
-#saving image
-my_img = cv2.merge((B, G, R)) 
-cv2.imwrite(sys.argv[1]+'_inverted.png', my_img)
-cv2.imshow(sys.argv[1]+'_inverted.png', my_img)     
+    #saving image
+    my_img = cv2.merge((B, G, R)) 
+    cv2.imwrite(sys.argv[1]+'_inverted.png', my_img)
+    cv2.imshow(sys.argv[1]+'_inverted.png', my_img)     
 
+#if in grayscale or binary
+else:    
+    # open in grayscale
+    img = cv2.imread(sys.argv[1],cv2.IMREAD_GRAYSCALE) 
+    my_img = np.array([S-x for x in img])
+    cv2.imwrite(sys.argv[1]+'_inverted.png', my_img)
+    cv2.imshow(sys.argv[1]+'_inverted.png', my_img) 
+     
 
 
 
